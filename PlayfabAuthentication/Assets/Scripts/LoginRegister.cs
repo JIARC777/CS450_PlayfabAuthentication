660ad41a49b60c934091203a997753a216d52d6e
Jeremy Ivan <59781543+JeremyI@users.noreply.github.com>
2020-10-27 08:28:42 -0700

    Build

---
+++ PlayfabAuthentication/Assets/Scripts/LoginRegister.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PlayFab;
+using PlayFab.ClientModels;
+using TMPro;
+using UnityEngine.Events;
+
+
+public class LoginRegister : MonoBehaviour
+{
+    public UnityEvent onLoggedIn;
+
+    public TMP_InputField usernameInput;
+    public TMP_InputField passwordInput;
+
+    public TextMeshProUGUI displayText;
+
+    [HideInInspector]
+    public string playFabId;
+    public static LoginRegister instance;
+    private void Awake()
+    {
+        instance = this;
+    }
+    // Start is called before the first frame update
+    public void OnRegister()
+    {
+        RegisterPlayFabUserRequest registerRequest = new RegisterPlayFabUserRequest { 
+            Username = usernameInput.text, 
+            DisplayName = usernameInput.text, 
+            Password = passwordInput.text, 
+            RequireBothUsernameAndEmail = false 
+        };
+
+        PlayFabClientAPI.RegisterPlayFabUser(registerRequest, 
+            result =>
+            {
+                Debug.Log(result.PlayFabId);
+                SetDisplayText("Account Created", Color.green);
+            },
+            error => {
+                Debug.Log(error.ErrorMessage);
+                SetDisplayText("There Was An Error. Please Try Again", Color.red);
+            }
+        );
+        
+    }
+
+    public void OnLoginButton() 
+    {
+        LoginWithPlayFabRequest loginRequest = new LoginWithPlayFabRequest 
+        { 
+            Username = usernameInput.text, 
+            Password = passwordInput.text 
+        };
+        PlayFabClientAPI.LoginWithPlayFab(loginRequest,
+            result =>
+            {
+                SetDisplayText("Logged in as: " + result.PlayFabId, Color.green);
+
+                if (onLoggedIn != null)
+                    onLoggedIn.Invoke();
+                playFabId = result.PlayFabId;
+            },     
+            error => Debug.Log(error.ErrorMessage)
+        );
+
+    }
+
+    void SetDisplayText(string text, Color color)
+    {
+        displayText.text = text;
+        displayText.color = color;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
