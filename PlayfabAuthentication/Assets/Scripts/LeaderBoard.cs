2b2661dcde90930903578794f406afd19e6a5e90
Jeremy Ivan <59781543+JeremyI@users.noreply.github.com>
2020-11-03 08:41:00 -0700

    build

---
+++ PlayfabAuthentication/Assets/Scripts/LeaderBoard.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PlayFab;
+using PlayFab.ClientModels;
+using TMPro;
+
+public class LeaderBoard : MonoBehaviour
+{
+    public GameObject leaderboardCanvas;
+    public GameObject[] leaderboardEntries;
+
+    public static LeaderBoard instance;
+    private void Awake()
+    {
+        instance = this;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public void OnLoggedIn()
+    {
+        leaderboardCanvas.SetActive(true) ;
+        DisplayLeaderboard();
+    }
+    public void DisplayLeaderboard()
+    {
+        GetLeaderboardRequest getLeaderboardRequest = new GetLeaderboardRequest { StatisticName = "FastestTime", MaxResultsCount = 10 };
+        PlayFabClientAPI.GetLeaderboard(getLeaderboardRequest, result => UpdateLeaderboardUI(result.Leaderboard), error => Debug.Log(error.ErrorMessage));
+    }
+
+    void UpdateLeaderboardUI(List<PlayerLeaderboardEntry> leaderboard)
+    {
+        for (int x = 0; x < leaderboardEntries.Length; x++)
+        {
+
+            leaderboardEntries[x].SetActive(x < leaderboard.Count);
+            if (x >= leaderboard.Count) continue;
+            leaderboardEntries[x].transform.Find("PlayerName").GetComponent<TextMeshProUGUI>().text = (leaderboard[x].Position + 1) + ". " + leaderboard[x].DisplayName;
+            leaderboardEntries[x].transform.Find("ScoreText").GetComponent<TextMeshProUGUI>().text = (-(float)leaderboard[x].StatValue * 0.001f).ToString("F2");
+        }
+    }
+    public void SetLeaderboardEntry(int newScore)
+    {
+        ExecuteCloudScriptRequest request = new ExecuteCloudScriptRequest
+        {
+            FunctionName = "UpdateHighscore",
+            FunctionParameter = new { score = newScore }
+        };
+
+        PlayFabClientAPI.ExecuteCloudScript(request,
+            result => DisplayLeaderboard(),
+            error => Debug.Log(error.ErrorMessage)
+        ); 
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ PlayfabAuthentication/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+    private Rigidbody rig;
+
+    private float startTime;
+    private float timeTaken;
+
+    private int collectablesPicked;
+    public int maxCollectables = 10;
+
+    public GameObject playButton;
+    public TextMeshProUGUI curTextTime;
+
+    private bool isPlaying;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rig = GetComponent<Rigidbody>();    
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isPlaying)
+            return;
+        float x = Input.GetAxis("Horizontal") * speed;
+        float z = Input.GetAxis("Vertical") * speed;
+        rig.velocity = new Vector3(x, rig.velocity.y, z);
+        curTextTime.text = (Time.time - startTime).ToString("F2");
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Collectable"))
+        {
+            collectablesPicked++;
+            Destroy(other.gameObject);
+            if (collectablesPicked == maxCollectables)
+                End();
+        }
+    }
+
+    public void Begin()
+    {
+        playButton.SetActive(true);
+        startTime = Time.time;
+        isPlaying = true;
+    }
+
+    void End()
+    {
+        timeTaken = Time.time - startTime;
+        isPlaying = false;
+        LeaderBoard.instance.SetLeaderboardEntry(-Mathf.RoundToInt(timeTaken * 1000f));
+        Debug.Log(-Mathf.RoundToInt(timeTaken * 1000f));
+        
+    }
+}
